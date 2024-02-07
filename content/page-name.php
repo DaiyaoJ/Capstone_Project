6d04530308164d259737e9ef295866fd06592905
DaiyaoJ <93630214+DaiyaoJ@users.noreply.github.com>
2024-02-07 04:26:13 -0500

    Create folder then upload code

---
+++ content/page-name.php
@@ -0,0 +1,13 @@
+<div class="container-fluid" >
+    
+<h2>Home</h2>
+</div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+       <h4>Welcome to the server control panel!</h4> Note: For each linux services below, you need to enter a 4-digit authentication code from your phone in order to access them. You also need a code for the Admin page. 
+      </div>
+
+    </div>
+  </div>
+  
